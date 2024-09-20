6882d1bb3c5dc2131156fbccf28dbc480421840d
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-09-20 17:40:05 +0300

    fix: update game fields

--- game.py
+++ game.py
@@ -20,86 +20,84 @@ class Turn(Enum):
   O = "O"
 
 class Game:
-  round: int
-  player_x_score: int
-  player_o_score: int 
-  first_turn: Union[Turn, None]
-  player_turn: Union[Turn, None]
-  player_won: Union[Turn, None]
-  is_ended: bool 
-  is_blocked: bool
-  field: list[Union[Turn, None]]
+  __round: int
+  __player_x_score: int
+  __player_o_score: int 
+  __first_turn: Union[Turn, None]
+  __player_turn: Union[Turn, None]
+  __player_won: Union[Turn, None]
+  __is_ended: bool 
+  __is_blocked: bool
+  __field: list[Union[Turn, None]]
 
   def __init__(self):
-    self.round = 0
-    self.player1_score = 0
-    self.player2_score = 0
-    self.first_turn = None
-    self.player_turn = None
-    self.player_won = None
-    self.is_ended = False 
-    self.is_blocked = True
-    self.field = copy.deepcopy(DEFAULT_FIELD)
+    self.__round = 0
+    self.__player_x_score = 0
+    self.__player_o_score = 0
+    self.__first_turn = None
+    self.__player_turn = None
+    self.__player_won = None
+    self.__is_ended = False 
+    self.__is_blocked = True
+    self.__field = copy.deepcopy(DEFAULT_FIELD)
   
   def get_state(self) -> dict:
     return {
-      "round": self.round,
-      "player_x_score": self.player_x_score,
-      "player_o_score": self.player_o_score,
-      "first_turn": self.first_turn,
-      "player_turn":self.player_turn,
-      "is_ended": self.is_ended,
-      "is_blocked": self.is_blocked,
-      "field":self.field,
+      "round": self.__round,
+      "player_x_score": self.__player_x_score,
+      "player_o_score": self.__player_o_score,
+      "first_turn": self.__first_turn,
+      "player_turn": self.__player_turn,
+      "player_won": self.__player_won,
+      "is_ended": self.__is_ended,
+      "is_blocked": self.__is_blocked,
+      "field": self.__field,
     }
   
   def choose_player(self, player: Turn) -> None:
-    self.first_turn = player
-    self.player_turn = player
-    self.is_blocked = False
+    self.__first_turn = player
+    self.__player_turn = player
+    self.__is_blocked = False
 
   def reset_round(self) -> None:    
-    self.field = DEFAULT_FIELD
-    self.player_turn = self.first_turn
-    self.player_won = None
-    self.is_ended = False
-    self.is_blocked = False
+    self.__field = copy.deepcopy(DEFAULT_FIELD)
+    self.__player_turn = self.__first_turn
+    self.__player_won = None
+    self.__is_ended = False
+    self.__is_blocked = False
 
   def new_round(self) -> None:
-    self.first_turn = self.player_turn
-    self.field = DEFAULT_FIELD
-    self.player_won = None
-    self.is_ended = False
-    self.is_blocked = False
+    self.__round += 1
+    self.reset_round()
 
   def reset_game(self) -> None:
     self.__init__()
 
   def move(self, slot: int) -> None:
-    if self.field[slot] != None | self.is_blocked:
+    if self.__field[slot] != None | self.__is_blocked:
       return
     
-    self.field[slot] = self.player_turn
+    self.__field[slot] = self.__player_turn
 
     for combination in WINNING_COMBINATIONS:
-      if self.field[combination[0]] == self.field[combination[1]] == self.field[combination[2]] != None:
+      if self.__field[combination[0]] == self.__field[combination[1]] == self.__field[combination[2]] != None:
         
-        self.player_won = self.player_turn
-        self.is_ended = True
-        self.is_blocked = True
+        self.__player_won = self.__player_turn
+        self.__is_ended = True
+        self.__is_blocked = True
 
-        if self.player_won == Turn.X:
-          self.player_x_score += 1
-          self.player_turn = Turn.O
+        if self.__player_won == Turn.X:
+          self.__player_x_score += 1
+          self.__player_turn = Turn.O
 
-        if self.player_won == Turn.O:
-          self.player_o_score += 1
-          self.player_turn = Turn.X
+        if self.__player_won == Turn.O:
+          self.__player_o_score += 1
+          self.__player_turn = Turn.X
 
         return
 
-    if self.player_turn == Turn.X:
-      self.player_turn = Turn.O
+    if self.__player_turn == Turn.X:
+      self.__player_turn = Turn.O
   
-    if self.player_turn == Turn.O:
-      self.player_turn = Turn.X
+    if self.__player_turn == Turn.O:
+      self.__player_turn = Turn.X
