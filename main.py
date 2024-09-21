583fc482c63101a0dc37eaf6e6fb496166c4f1c1
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-09-21 18:22:48 +0300

    feat: update layout

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from kivy.app import App
+from kivy.uix.popup import Popup
 from kivy.core.window import Window 
 from kivy.uix.boxlayout import BoxLayout
+
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 from game import Game
 
 class TicTacToeGame(BoxLayout):
-  pass
+  game: Game
+
+  def __init__(self, **kwargs):
+    super(TicTacToeGame, self).__init__(**kwargs)
+    self.game = Game()
+
+  def update(self):
+    state = self.game.get_state()
+    pass
+
 
 class TicTacToeApp(App):
   def build(self):
-    Window.size = (400, 500)
+    Window.size = (400, 600)
     Window.minimum_width = 400
-    Window.minimum_height = 500
-    Window.set_title("Tic Tac Toe Game")
-    Window.clearcolor = (10/255, 10/255, 35/255, 1)
-
+    Window.minimum_height = 600
     return TicTacToeGame()
 
 if __name__ == '__main__':
