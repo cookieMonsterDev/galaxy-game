9bd6f07f46271d83e87fca5713b702d536687af5
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-09-21 15:55:53 +0300

    feat: add layout

--- main.py
+++ main.py
@@ -1,55 +1,21 @@
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.core.window import Window
+from kivy.core.window import Window 
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
 
 from game import Game
 
-class PlayField(GridLayout):
-  def __init__(self, game: Game, **kwargs):
-    super().__init__(**kwargs)
-    self.cols = 3
-    self.game = game
-
-    field = self.game.get_state()['field']
- 
-    for index, item in enumerate(field):
-      print(index, item)
-      button_text = ""
-
-      if item:
-        button_text = item
-      
-      button = Button(text=button_text)
-      self.add_widget(button)
-
-
-class KeyPad(GridLayout):
-  def __init__(self, game: Game, **kwargs):
-    super().__init__(**kwargs)
-    self.cols = 2
-    self.game = game
-
-    reset_game_button = Button(text="Reset Game")
-    reset_round_button = Button(text="Reset Round")
-
-    reset_game_button.bind(on_press=self.game.reset_game)
-    reset_round_button.bind(on_press=self.game.reset_round)
-
-    self.add_widget(reset_game_button)
-    self.add_widget(reset_round_button)
-
+class TicTacToeGame(BoxLayout):
+  pass
 
 class TicTacToeApp(App):
   def build(self):
-    game = Game()
-    root_layout = BoxLayout(orientation="vertical")
+    Window.size = (400, 500)
+    Window.minimum_width = 400
+    Window.minimum_height = 500
+    Window.set_title("Tic Tac Toe Game")
+    Window.clearcolor = (10/255, 10/255, 35/255, 1)
 
-    root_layout.add_widget(PlayField(game))
-    root_layout.add_widget(KeyPad(game))
-
-    return root_layout 
+    return TicTacToeGame()
 
 if __name__ == '__main__':
     TicTacToeApp().run()
