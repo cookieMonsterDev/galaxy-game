14f117e207ca3f79d68b61062220aae1181d2329
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-09-23 13:47:47 +0300

    ch

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
+from enum import Enum
 from kivy.app import App
-from kivy.uix.popup import Popup
-from kivy.core.window import Window 
-from kivy.uix.boxlayout import BoxLayout
-
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.core.window import Window 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 
-from game import Game
+
+from src.game import Game
+
+class Color(Enum):
+  X = (255/255, 51/255, 51/255, 1)
+  O = (25/255, 140/255, 255/255, 1)
+  N = (255/255, 255/255, 255/255, 1)
 
 class TicTacToeGame(BoxLayout):
   game: Game
 
   def __init__(self, **kwargs):
     super(TicTacToeGame, self).__init__(**kwargs)
+
     self.game = Game()
 
-  def update(self):
-    state = self.game.get_state()
+    print(self.game.get_state())
+
+  def render_info_bar(self):
+    pass
+
+  def render_field(self):
+    pass
+
+  def render_keypad(self):
     pass
 
 
