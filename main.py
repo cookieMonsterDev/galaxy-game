b22a6ce508a2f29f8e8ee68e41eda9965b06cc55
cookieMonsterDev <mykhailo.toporkov@gmail.com>
2024-09-24 10:47:32 +0300

    ch

--- main.py
+++ main.py
@@ -1,45 +1,14 @@
-from enum import Enum
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
 from kivy.core.window import Window 
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
 
-
-from src.game import Game
-
-class Color(Enum):
-  X = (255/255, 51/255, 51/255, 1)
-  O = (25/255, 140/255, 255/255, 1)
-  N = (255/255, 255/255, 255/255, 1)
-
-class TicTacToeGame(BoxLayout):
-  game: Game
-
-  def __init__(self, **kwargs):
-    super(TicTacToeGame, self).__init__(**kwargs)
-
-    self.game = Game()
-
-    print(self.game.get_state())
-
-  def render_info_bar(self):
-    pass
-
-  def render_field(self):
-    pass
-
-  def render_keypad(self):
-    pass
-
+from src import Game
 
 class TicTacToeApp(App):
   def build(self):
     Window.size = (400, 600)
     Window.minimum_width = 400
     Window.minimum_height = 600
-    return TicTacToeGame()
+    return Game()
 
 if __name__ == '__main__':
     TicTacToeApp().run()
--- src/game.py
+++ src/game.py
@@ -1,103 +1,138 @@
-from enum import Enum
-from typing import Union
 import copy
+from enum import Enum
+
+from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics import Color, Rectangle
+from kivy.uix.gridlayout import GridLayout
+
+from src.colors import MAIN_TEXT_COLOR, BACKGROUND_COLOR 
+from .buttons import KeypadButton, BoardButton
 
 WINNING_COMBINATIONS = (
-  (0, 1, 2),
-  (3, 4, 5),
-  (6, 7, 8),
-  (0, 3, 6),
-  (1, 4, 7),
-  (2, 5, 8),
-  (0, 4, 8),
-  (2, 4, 6)
+    (0, 1, 2),
+    (3, 4, 5),
+    (6, 7, 8),
+    (0, 3, 6),
+    (1, 4, 7),
+    (2, 5, 8),
+    (0, 4, 8),
+    (2, 4, 6)
 )
 
-DEFAULT_FIELD = [None] * 9
+DEFAULT_BOARD = [None] * 9
 
-class Turn(Enum):
-  X = "X"
-  O = "O"
+# class PlayerName(Enum):
+#   X = "X"
+#   O = "O"
 
-class Game:
-  __round: int
-  __player_x_score: int
-  __player_o_score: int 
-  __first_turn: Union[Turn, None]
-  __player_turn: Union[Turn, None]
-  __player_won: Union[Turn, None]
-  __is_ended: bool 
-  __is_blocked: bool
-  __field: list[Union[Turn, None]]
+# class Game:
+#   def __init__(self):
+#     self.round = 0
+#     self.player_x_score = 0
+#     self.player_o_score = 0
+#     self.first_turn = None
+#     self.player_turn = None
+#     self.player_won = None
+#     self.is_ended = False
+#     self.is_blocked = False
+#     self.field = copy.deepcopy(DEFAULT_FIELD)
 
-  def __init__(self):
-    self.__round = 0
-    self.__player_x_score = 0
-    self.__player_o_score = 0
-    self.__first_turn = None
-    self.__player_turn = None
-    self.__player_won = None
-    self.__is_ended = False 
-    self.__is_blocked = True
-    self.__field = copy.deepcopy(DEFAULT_FIELD)
-  
-  def get_state(self) -> dict:
-    return {
-      "round": self.__round,
-      "player_x_score": self.__player_x_score,
-      "player_o_score": self.__player_o_score,
-      "first_turn": self.__first_turn,
-      "player_turn": self.__player_turn,
-      "player_won": self.__player_won,
-      "is_ended": self.__is_ended,
-      "is_blocked": self.__is_blocked,
-      "field": self.__field,
-    }
-  
-  def choose_player(self, player: Turn) -> None:
-    self.__first_turn = player
-    self.__player_turn = player
-    self.__is_blocked = False
+#   def choose_player(self, player: PlayerName) -> None:
+#     self.first_turn = player
+#     self.player_turn = player
+#     self.is_blocked = False
 
-  def reset_round(self) -> None:    
-    self.__field = copy.deepcopy(DEFAULT_FIELD)
-    self.__player_turn = self.__first_turn
-    self.__player_won = None
-    self.__is_ended = False
-    self.__is_blocked = False
+#   def reset_round(self) -> None:
+#     self.field = copy.deepcopy(DEFAULT_FIELD)
+#     self.player_turn = self.first_turn
+#     self.player_won = None
+#     self.is_ended = False
+#     self.is_blocked = False
 
-  def new_round(self) -> None:
-    self.__round += 1
-    self.reset_round()
+#   def new_round(self) -> None:
+#     self.round += 1
+#     self.reset_round()
 
-  def reset_game(self) -> None:
-    self.__init__()
+#   def reset_game(self) -> None:
+#     print('reset')
+#     self.__init__()
 
-  def move(self, slot: int) -> None:
-    if self.__field[slot] != None | self.__is_blocked:
-      return
+#   def move(self, slot: int) -> None:
+#     if self.field[slot] is not None or self.is_blocked:
+#       return
+
+#     self.field[slot] = self.player_turn
+
+#     for combination in WINNING_COMBINATIONS:
+#       if all(self.field[index] == self.player_turn for index in combination):
+#         self.player_won = self.player_turn
+#         self.is_ended = True
+#         self.is_blocked = True
+
+#         if self.player_won == PlayerName.X.value:
+#           self.player_x_score += 1
+#           self.player_turn = PlayerName.O.value
+#         else:
+#           self.player_o_score += 1
+#           self.player_turn = PlayerName.X.value
+#         return
+
+#     if self.player_turn == PlayerName.X.value:
+#         self.player_turn = PlayerName.O.value
+#     else:
+#         self.player_turn = PlayerName.X.value
+
+
+
+class Game(BoxLayout):
+  def __init__(self, **kwargs):
+    super(Game, self).__init__(**kwargs)
+    self.padding = 20
+    self.orientation = 'vertical'
+    self.bind(pos=self.update_canvas, size=self.update_canvas)
+
+
+  def update_game_canvas(self, *args):
+    self.canvas.before.clear()
+    with self.canvas.before:
+      Color(*BACKGROUND_COLOR)
+      Rectangle(pos=self.pos, size=self.size)
+
+
+  def render_board(self):
+    board = GridLayout(cols=3, spacing=4, size_hint_y=0.7)
+
+    with board.canvas.before:
+      Color(*MAIN_TEXT_COLOR)
+      Rectangle(
+        pos=(board.pos[0] + 2, board.pos[1] + 2),
+        size=(board.width - 4, board.height - 4) 
+      )
     
-    self.__field[slot] = self.__player_turn
+    for item in self.board:
+      button = BoardButton(text=item if item != None else "")
+      board.add_widget(button)
 
-    for combination in WINNING_COMBINATIONS:
-      if self.__field[combination[0]] == self.__field[combination[1]] == self.__field[combination[2]] != None:
-        
-        self.__player_won = self.__player_turn
-        self.__is_ended = True
-        self.__is_blocked = True
+    self.add_widget(board)
 
-        if self.__player_won == Turn.X:
-          self.__player_x_score += 1
-          self.__player_turn = Turn.O
 
-        if self.__player_won == Turn.O:
-          self.__player_o_score += 1
-          self.__player_turn = Turn.X
+  def update_board_canvas(self, board, *args):
+    board.canvas.before.clear()
 
-        return
+    with board.canvas.before:
+        Color(*MAIN_TEXT_COLOR)
+        Rectangle(
+          pos=(board.x + 2, board.y + 2), 
+          size=(board.width - 4, board.height - 4) 
+        )
 
-    if self.__player_turn == Turn.X:
-      self.__player_turn = Turn.O
-  
-    if self.__player_turn == Turn.O:
-      self.__player_turn = Turn.X
+  def render_keypad(self):
+    keypad = BoxLayout(spacing=20, size_hint_y=0.1, padding=(0, 30, 0, 0))
+
+    left_button = KeypadButton(text="Reset Game")
+    right_button = KeypadButton(text="Reset Round")
+
+    keypad.add_widget(left_button)
+    keypad.add_widget(right_button)
+
+    self.add_widget(keypad)
